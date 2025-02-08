37ff192c26dfba78533bac3df9570ce6683803a5
Jesús Guadalupe Macías Amador <47731561+jesusamador-dev@users.noreply.github.com>
2025-02-08 05:58:03 -0600

    Update AuthRepository.ts

--- src/data/repositories/AuthRepository.ts
+++ src/data/repositories/AuthRepository.ts
@@ -5,7 +5,7 @@ import { axiosGlobal } from 'mf_mesacyc_dashboards_common/axios';
 export class AuthRepository implements IAuthRepository {
 
   async login(request: IRequestLogin): Promise<String> {       
-    const response: {data: String} = await axiosGlobal.post(`${process.env.REACT_APP_MIDDLEWARE}cobranza-credito/investigacion-cobranza/ffm/lbd-middleware-seguridadffm/torrecontrol/auth/token`, request, { 'Content-Type': 'application/json' });
+    const response: {data: String} = await axiosGlobal.post(`https://proxy-reverse-production.up.railway.app/torre-control/cobranza-credito/investigacion-cobranza/ffm/lbd-middleware-seguridadffm/torrecontrol/auth/token`, request, { 'Content-Type': 'application/json' });
     return response.data;
   }
 }
